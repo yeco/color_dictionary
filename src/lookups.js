40470aa2f566620bce91a97c2584f080345ca2c5
Jasson Cascante <jasson.cascante@edify.cr>
2014-06-26 16:17:48 -0600

    Added lookup arrays

--- src/lookups.js
+++ src/lookups.js
@@ -1,9 +1,9 @@
 var hexLookup = [];
 for (var i = color_dictionary.length - 1; i >= 0; i--) {
-    hexLookup[array[i].hex] = array[i];
+    hexLookup[color_dictionary[i].hex] = color_dictionary[i];
 };
 
 var nameLookup = [];
 for (var i = color_dictionary.length - 1; i >= 0; i--) {
-    nameLookup[array[i].name] = array[i];
+    nameLookup[color_dictionary[i].name] = color_dictionary[i];
 };
